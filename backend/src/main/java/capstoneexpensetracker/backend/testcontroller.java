42d6f5d42c4562f95865cd31fbde42d92d6b87c0
Zeshan Shahid <z.shahid@hotmail.de>
2022-11-18 12:53:01 +0100

    connect backend and frontend (add axios, new controller)

---
+++ backend/src/main/java/capstoneexpensetracker/backend/testcontroller.java
@@ -0,0 +1,15 @@
+package capstoneexpensetracker.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/hello")
+public class testcontroller {
+
+    @GetMapping
+    String hello(){
+        return "Hello";
+    }
+}
