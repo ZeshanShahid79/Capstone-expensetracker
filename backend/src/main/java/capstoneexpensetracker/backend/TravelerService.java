6ae11ec8f53afa7268a8ac29a7e3d3f83875dfee
Zeshan Shahid <z.shahid@hotmail.de>
2022-11-21 16:54:46 +0100

    refactor code wit reformatter

--- backend/src/main/java/capstoneexpensetracker/backend/TravelerService.java
+++ backend/src/main/java/capstoneexpensetracker/backend/TravelerService.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class TravelerService {
     private final TravelerRepository travelerRepository;
+
     public List<Traveler> displayTravelerList() {
         return travelerRepository.findAll();
     }
