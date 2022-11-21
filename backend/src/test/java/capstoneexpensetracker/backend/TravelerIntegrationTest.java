e6d2230a12149353f28a6007afc8e7c9d2926d0b
Zeshan Shahid <z.shahid@hotmail.de>
2022-11-21 14:31:45 +0100

    add integration test

---
+++ backend/src/test/java/capstoneexpensetracker/backend/TravelerIntegrationTest.java
@@ -0,0 +1,31 @@
+package capstoneexpensetracker.backend;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TravelerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DirtiesContext
+    @Test
+    void getAllTravelersAndExpectEmptyList() throws Exception{
+    mockMvc.perform(MockMvcRequestBuilders.get("/api/hello"))
+            .andExpect(status().isOk())
+            .andExpect(content().json("[]"));
+    }
+}
