8adeeb8a3f12491326397e9caa4b009fd472d7d1
Zeshan Shahid <z.shahid@hotmail.de>
2022-11-25 17:10:55 +0100

    add and implement TravelerGroup Form to add and display Groups, refactor whole code to use all states and fetches in each component

--- backend/src/main/java/capstoneexpensetracker/backend/travelergroup/NewTravelerGroup.java
+++ backend/src/main/java/capstoneexpensetracker/backend/travelergroup/NewTravelerGroup.java
@@ -7,6 +7,7 @@ import java.util.List;
 public record NewTravelerGroup(
         String description,
         List<Traveler> travelerList
+
 ) {
 
 
--- frontend/src/TravelerGroup/TravelerGroupModel/TravelerGroupModel.ts
+++ frontend/src/TravelerGroup/TravelerGroupModel/TravelerGroupModel.ts
@@ -5,3 +5,4 @@ export type TravelerGroupModel = {
     travelerList: TravelerModel[],
     id: string
 }
+
