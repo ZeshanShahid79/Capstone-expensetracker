7e6b4c0941d8c9bc9221c5a4b1fbc5dd2e53b321
Zeshan Shahid <z.shahid@hotmail.de>
2022-11-30 16:22:14 +0100

    refactor test

--- backend/src/test/java/capstoneexpensetracker/backend/ReactRoutingForwardingTest.java
+++ backend/src/test/java/capstoneexpensetracker/backend/ReactRoutingForwardingTest.java
@@ -1,6 +1,5 @@
 package capstoneexpensetracker.backend;
 
-import capstoneexpensetracker.backend.ReactRoutingForwarding.ReactRoutingPathResourceResolver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
@@ -11,43 +10,37 @@ import java.io.IOException;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class ReactRoutingPathResourceResolverTest {
-
+class ReactRoutingForwardingTest {
     @Test
     void expectRelativeResource_ifItExists() throws IOException {
-
         // GIVEN
-        var resolver = new ReactRoutingPathResourceResolver();
+        var resolver = new ReactRoutingForwarding.ReactRoutingPathResourceResolver();
         var location = mock(Resource.class);
         var relativeLocation = mock(Resource.class);
         var resourcePath = "index.html";
         when(location.createRelative(resourcePath)).thenReturn(relativeLocation);
         when(relativeLocation.exists()).thenReturn(true);
         when(relativeLocation.isReadable()).thenReturn(true);
-
         // WHEN
         var actual = resolver.getResource(resourcePath, location);
-
         // THEN
         Assertions.assertEquals(relativeLocation, actual);
     }
 
     @Test
     void expectIndexHtml_ifRequestedResourceDoesNotExist() throws IOException {
-
         // GIVEN
-        var resolver = new ReactRoutingPathResourceResolver();
+        var resolver = new ReactRoutingForwarding.ReactRoutingPathResourceResolver();
         var location = mock(Resource.class);
         var relativeLocation = mock(Resource.class);
         var resourcePath = "index.html";
         when(location.createRelative(resourcePath)).thenReturn(relativeLocation);
         when(relativeLocation.exists()).thenReturn(false);
-
         // WHEN
         var actual = resolver.getResource(resourcePath, location);
-
         // THEN
         ClassPathResource expected = new ClassPathResource("static/index.html");
         Assertions.assertEquals(expected, actual);
     }
+
 }
